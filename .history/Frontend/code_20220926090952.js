3e4647f0ecd89540cec0c591bc9d70e1edbeab33
mangos1234 <109079556+mangos1234@users.noreply.github.com>
2022-09-26 11:17:21 +0100

    navbar added to home

---
+++ .history/Frontend/code_20220926090952.js
@@ -0,0 +1,44 @@
+var registerContainer = document.querySelector(".registerContainer");
+var registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", function(){
+    registerContainer.style.display = "flex";
+})
+
+var loginBtn = document.getElementById("loginBtn");
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var errortext = document.querySelector("#loginerror");
+
+loginBtn.addEventListener("click", function(){
+    if (username.value == "" || password.value == "")
+    {
+        errortext.style.display = "block";
+        errortext.innerHTML = "Please enter a username and password";
+    }
+    else{
+        errortext.style.display = "none";
+        window.location.href = "home.html";
+        console.log("send to ewa")
+    }
+})
+
+var makeAccountBtn = document.getElementById("makeAccountBtn");
+
+var registeremail = document.getElementById("registerEmail");
+var registerusername = document.getElementById("registerUsername");
+var registerpassword = document.getElementById("registerPassword");
+var registererrortext = document.querySelector("#registererror");
+
+makeAccountBtn.addEventListener("click", function(){
+    if (registerusername.value == "" || registerpassword.value == "" || registeremail.value == "")
+    {
+        registererrortext.style.display = "block";
+        registererrortext.innerHTML = "Please enter a username, password and email";
+    }
+    else{
+        registererrortext.style.display = "none";
+        window.location.href = "home.html";
+        console.log("send to ewa")
+    }
+})
