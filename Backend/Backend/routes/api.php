3bab9e7f31c0d2f499a5fa56074ddda7c9ef269a
mangos1234 <109079556+mangos1234@users.noreply.github.com>
2022-09-27 11:07:40 +0100

    .

--- Backend/Backend/routes/api.php
+++ Backend/Backend/routes/api.php
@@ -9,7 +9,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //User Routes
+Route::get('/users/login/{username}/{pasword}', [userController::class, 'CheckLogin']); //Checks Login Credentials // done
 
+Route::post('/users/register/{username}/{pasword}/{email}', [userController::class, 'RegisterUser']); //Creates new user if user with same username doesn't exist yet //done
 
 Route::get('/users/sendemail/{email}', [userController::class, 'SendEmail']); //Sends email for resetting password
 
