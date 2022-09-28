8c2dba4402d4ae955726f593e3e28fe8bb64fcea
Ewa-Podolak <112617120+Ewa-Podolak@users.noreply.github.com>
2022-09-28 13:10:10 +0100

    display comments

--- Backend/Backend/app/Models/comment.php
+++ Backend/Backend/app/Models/comment.php
@@ -14,14 +14,65 @@ class comment extends Model
 
     public function GetComments($postid, $page)
     {
-        $allcomments = $this::orderby('created_at');
-        return $allcomments->get();
+        $allfavouritecomments = $this::where('postid', $postid)->where('favourited', 1)->orderby('created_at')->get();
+        $allnotfavouritecomments = $this::where('postid', $postid)->where('favourited', 0)->orderby('created_at')->get();
+
+        $favouritecommentarray = [];
+        $notfavouritecommentarray = [];
+        
+        foreach($allfavouritecomments as $comment)
+        {
+            $comment['votes'] = $this->Votes($comment->commentid);
+            array_push($favouritecommentarray, ['commentid'=>$comment->commentid, 
+                                                'comment'=>$comment->comment, 
+                                                'votes'=>$comment->votes, 
+                                                'favourited'=>true,
+                                                'created_at'=>$comment->created_at, 
+                                                'username'=>user::where('userid', $comment->userid)->first()->username]);
+        }
+
+        foreach($allnotfavouritecomments as $comment)
+        {
+            $comment['votes'] = $this->Votes($comment->commentid);
+            $comment['votes'] = $this->Votes($comment->commentid);
+            array_push($notfavouritecommentarray, ['commentid'=>$comment->commentid, 
+                                                'comment'=>$comment->comment, 
+                                                'votes'=>$comment->votes, 
+                                                'favourited'=>false,
+                                                'created_at'=>$comment->created_at, 
+                                                'username'=>user::where('userid', $comment->userid)->first()->username]);
+        }
+
+        $votes = array_column($favouritecommentarray, 'votes');
+        array_multisort($votes, SORT_DESC, $favouritecommentarray);
+
+        $votes = array_column($notfavouritecommentarray, 'votes');
+        array_multisort($votes, SORT_DESC, $notfavouritecommentarray);
+
+        $commentsarray = array_merge($favouritecommentarray, $notfavouritecommentarray);
+
+
+        if(count($commentsarray) + 1 == $page * 5)
+        {
+            return ['commentid' => null, 'comment' => null, 'votes' => null, 'favourited'=>null, 'created_at'=>null, 'usesrname'=>null];
+        }
+        else
+        {
+            $endarray = [];
+            for($x = ($page -1) * 5; $x < ($page * 5); $x++)
+            {
+                if($x < count($commentsarray))
+                    array_push($endarray, $commentsarray[$x]);
+            }
+        }
+
+        return $endarray;
     }
 
-    public function Votes($postid)
+    public function Votes($commentid)
     {
-        $upvotes = interaction::where('postid', $postid)->where('liked', 1)->get()->count();
-        $downvotes = interaction::where('postid', $postid)->where('liked', 0)->get()->count();
+        $upvotes = interaction::where('commentid', $commentid)->where('liked', 1)->get()->count();
+        $downvotes = interaction::where('commentid', $commentid)->where('liked', 0)->get()->count();
         return $upvotes-$downvotes;
     }
     
