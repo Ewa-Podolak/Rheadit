0fd1c2dcd3f7ce9cb71bbe17cbb0898238f3a698
Ewa-Podolak <112617120+Ewa-Podolak@users.noreply.github.com>
2022-10-10 14:04:43 +0100

    last push

--- Backend/Backend/database/seeders/communitySeeder.php
+++ Backend/Backend/database/seeders/communitySeeder.php
@@ -14,5 +14,9 @@ class communitySeeder extends Seeder
         community::insert(['userid' => 2, 'community' => 'Frontend', 'authority' => 'owner']);
         community::insert(['userid' => 3, 'community' => 'Backend', 'authority' => 'member']);
         community::insert(['userid' => 3, 'community' => 'Frontend', 'authority' => 'mod']);
+
+        community::insert(['userid' => 1, 'community' => 'homepage', 'authority' => 'member']);
+        community::insert(['userid' => 2, 'community' => 'homepage', 'authority' => 'member']);
+        community::insert(['userid' => 3, 'community' => 'homepage', 'authority' => 'member']);
     }
 }
